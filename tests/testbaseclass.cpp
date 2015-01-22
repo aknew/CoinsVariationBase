6d071c7eb2d9448e78a5cccdf2a7027c4658cba4
aknew <kazansky85@gmail.com>
2015-01-23 00:42:53 +0300

    add new row insertion  test
    it is failed now

--- tests/testbaseclass.cpp
+++ tests/testbaseclass.cpp
@@ -3,9 +3,9 @@
 #include <QString>
 #include <QDir>
 #include <QFile>
+#include "../src/CVBBaseProvider.h"
 
-
-const QString TestBaseFolder="testBase";
+const QString TestBaseFolder="testBase/";
 
 TestBaseClass::TestBaseClass(QObject *parent) :
     QObject(parent)
@@ -19,10 +19,33 @@ void TestBaseClass::initTestCase(){
     dir.removeRecursively();
     QDir().mkdir(TestBaseFolder);
 
-    QFile::copy(":/base.sqlite",TestBaseFolder+"/base.sqlite");
-    QFile::copy(":/struct.json",TestBaseFolder+"/struct.json");
+    QFile::copy(":/base.sqlite",TestBaseFolder+"base.sqlite");
+    QFile::copy(":/struct.json",TestBaseFolder+"struct.json");
 }
 
 void TestBaseClass::testRowInsertion(){
-        //QCOMPARE(query.value(0).toInt(),1);
+    CVBBaseProvider *baseProvider = new CVBBaseProvider(TestBaseFolder);
+    baseProvider->startLevel();
+    int before = baseProvider->currentNode()->model->rowCount();
+
+    baseProvider->currentNode()->selectItemWithIndex(CVBSqlNode::kNewRowIndex);
+
+    QVariantMap returnedMap;
+    returnedMap["avers"]="avers test description";
+    returnedMap["edge"]="//////////";
+    returnedMap["firstYear"]="1900";
+    returnedMap["issue"]="test issue";
+    returnedMap["lastYear"]="2000";
+    returnedMap["metal"]="test metall";
+    returnedMap["nominal"]="test nominal";
+    returnedMap["revers"]="avers test description";
+    returnedMap["weigth"]="0";
+
+    baseProvider->currentNode()->model->setSelectedItem(returnedMap);
+    QCOMPARE(baseProvider->currentNode()->model->rowCount(),before+1);
+
+    delete baseProvider;
+    baseProvider = new CVBBaseProvider(TestBaseFolder);
+    baseProvider->startLevel();
+    QCOMPARE(baseProvider->currentNode()->model->rowCount(),before+1);
 }
