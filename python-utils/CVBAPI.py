113b5be383e718dcffe56100c8574b031d294730
Alexander Kazanskiy <kazansky85@gmail.com>
2015-05-12 10:54:47 +0300

    Utils: saving varieties
    Now we can save all variety information - source ref, pictures with
    coping files etc
    And I didn't  add CVBApi in previous commit, so it is unworkable

--- python-utils/CVBAPI.py
+++ python-utils/CVBAPI.py
@@ -2,26 +2,42 @@ __author__ = 'aknew'
 
 
 import hashlib
+import uuid
+import shutil
 
+# init DB connection
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, String
 
-class Variety(object):
-    typeId = ""
-    id = ""
-    varityType = "Обычная разновидность"
-    year = ""
-    mintmark = ""
-    mint = ""
-    avers = ""
-    revers = ""
-    edge = ""
-    rarity = ""
-    comment = ""
-    pictures = []
-    references = []
+base_root_dir = "../CoinsBase.base/"
+
+engine = create_engine('sqlite:///' + base_root_dir + "base.sqlite", echo=True)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class Variety(Base):
+    __tablename__ = 'Variaties'
+    typeId = Column(String)
+    id = Column(String,primary_key = True)
+    varityType = Column(String)
+    year = Column(String)
+    mintmark = Column(String)
+    mint = Column(String)
+    avers = Column(String)
+    revers = Column(String)
+    edge = Column(String)
+    # FIXME: need rename column into the base
+    rarity = Column("price", String)
+    comment = Column(String)
 
     def __init__(self):
         self.pictures = []
         self.references = []
+        self.id = uuid.uuid1().__str__()
 
     # def __repr__(self):
     # return "<Test a:%s b:%s>" % (self.a, self.b)
@@ -30,22 +46,46 @@ class Variety(object):
     #     return "From str method of Test: a is %s, b is %s" % (self.a, self.b)
 
 
-class SourceRef(object):
-    varID = ""
-    srid = ""
-    number = ""
-    rarity = ""
-    comment = ""
+class SourceRef(Base):
+    __tablename__ = 'SourceRef'
+    varID = Column(String, primary_key=True)
+    srid = Column(String, primary_key=True)
+    number = Column(String)
+    rarity = Column(String)
+    comment = Column(String)
 
 
-class CoinPicture(object):
-    table = "Variaties"
+class CoinPicture(Base):
+    __tablename__ = 'Images'
+    id = Column(String,primary_key = True)
+    comment = Column(String)
+    source = Column(String)
+    relid = Column(String)
+    table = Column(String)
+
 
     def __init__(self, filename, source, varid):
         self.path = filename
         self.source = source
         # fixme: Need add relation table, but now I always works with Variaties
-        self.relation = varid
+        self.table = "Variaties"
+        self.relid = varid
         # try to get md5
         pictfile = open(self.path, 'rb')
         self.id = hashlib.md5(pictfile.read()).hexdigest()
+
+def loadVarities(type = ""):
+    if "" == type:
+        return session.query(Variety).all()
+    else:
+        session.query(Variety).filter_by(typeId=type).all()
+
+def saveVariaties(varietiesList):
+    session.add_all(varietiesList)
+    for variety in varietiesList:
+        session.add_all(variety.references)
+        session.add_all(variety.pictures)
+        for pict in variety.pictures:
+            shutil.copy(pict.path, base_root_dir + "images/" + pict.id + ".jpg")
+
+    session.commit()
