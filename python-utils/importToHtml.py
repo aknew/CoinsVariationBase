171a51de16d0c58ba9b2ee8491ade8b828381bc1
Alexander Kazanskiy <kazansky85@gmail.com>
2015-06-17 01:55:05 +0300

    add import base to html script
    first version - without pictures, references and now it imports all
    varieties

---
+++ python-utils/importToHtml.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+__author__ = 'aknew'
+
+import CVBAPI
+
+# vrp
+typeId = "" #"a5886730-bb5d-43f4-a172-52294bc18952"
+
+varietyList = CVBAPI.loadVarities(typeId)
+
+tableOfContents = "<table border=\"1\">\
+   <caption><h1>Список разновидностей<h1></caption> \
+   <tr> \
+    <th>Год</th>\
+    <th>Буквы</th>\
+    <th>Описание разновидности</th>\
+    <th>Тип разновидности</th>\
+   </tr>"
+
+fullDescription = "<h1>Подробное описание разновидностей:</h1>"
+
+for variety in varietyList:
+    tableOfContents = tableOfContents + "<tr>"
+    fullDescription = fullDescription + "<hr> <a id=\"" + variety.id+"\"></a>"
+
+    fullDescription = fullDescription + "<b> Тип разновидности: </b>" + variety.varityType + "<p>"
+
+    tableOfContents = tableOfContents + "<td>" + str(variety.year) + "</td>"
+    fullDescription = fullDescription + "<b> Год: </b>" + str(variety.year)
+
+    tableOfContents = tableOfContents + "<td>" + variety.mintmark + "</td>"
+    fullDescription = fullDescription + "<b> Буквы: </b>" + variety.mintmark + "<p>"
+
+    tableOfContents = tableOfContents + "<td>" + "<a href=\"#" + variety.id + "\">"
+
+    if "" != variety.avers:
+        tableOfContents = tableOfContents + "<b>Л.С.:</b>" + variety.avers + "<p>"
+        fullDescription = fullDescription + "<b>Л.С.:</b>" + variety.avers + "<p>"
+
+    if "" != variety.revers:
+        tableOfContents = tableOfContents + "<b>О.С.:</b>" + variety.revers
+        fullDescription = fullDescription + "<b>О.С.:</b>" + variety.revers + "<p>"
+
+    if "" == variety.avers and "" == variety.revers:
+        tableOfContents = tableOfContents + "перейти к подробному описанию"
+
+    if "" != variety.comment and variety.comment:
+        fullDescription = fullDescription + "<b>Комментарий:</b>" + variety.comment + "<p>"
+
+    #todo: add refList and pictures
+
+
+    tableOfContents = tableOfContents +  "</td>"
+
+    tableOfContents = tableOfContents + "<td>" + variety.varityType + "</td>"
+    tableOfContents = tableOfContents + "</tr>"
+
+tableOfContents = tableOfContents + "</table>"
+
+result =  open("import/result.html", "w")
+
+#fixme: title should be generated from type
+result.write("<!DOCTYPE HTML> \
+<html> \
+ <head> \
+  <meta charset=\"cp1251\"> \
+  <title>Полушки ВРП</title> \
+ </head> \
+ <body>");
+#todo: add table of context
+result.write(tableOfContents)
+result.write(fullDescription)
+
+#todo: add references
+
+result.close();
