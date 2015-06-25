c189de1d46a6b7e0fcbb5fc86cbaa753b307bf0f
Alexander Kazanskiy <kazansky85@gmail.com>
2015-06-26 00:18:07 +0300

    import: new struct

--- python-utils/importToHtml.py
+++ python-utils/importToHtml.py
@@ -10,67 +10,63 @@ path = "import/"
 
 varietyList = CVBAPI.loadVarities(typeId)
 
-tableOfContents = "Список разновидностей|Год||Буквы|Описание разновидности|Тип разновидности"
+result = open(path+"result.md", "wb")
 
-fullDescription = "#Подробное описание разновидностей:"
+currentYear = -1
+currentMM = "";
 
 for variety in varietyList:
-    fullDescription = fullDescription + "#" + variety.id
+    if variety.year != currentYear or currentMM != variety.mintmark:
 
-    fullDescription = fullDescription + "*Тип разновидности:* " + variety.varityType + "\n\n"
+        currentYear = variety.year
+        currentMM = variety.mintmark
+        result.write(("##" + str(variety.year) + " " + variety.mintmark + "\n\n").encode('utf-8'))
 
-    tableOfContents = tableOfContents + str(variety.year) + "|"
-    fullDescription = fullDescription + "* Год: *" + str(variety.year)
+    result.write("---\n\n".encode('utf-8'))
 
-    tableOfContents = tableOfContents + "|" + variety.mintmark + "|"
-    fullDescription = fullDescription + " *Буквы:* " + variety.mintmark + "\n\n"
-
-    tableOfContents = tableOfContents + "|["
+    result.write(("**Тип разновидности:** " + variety.varityType + "\n\n").encode('utf-8'))
 
     if "" != variety.avers:
-        tableOfContents = tableOfContents + "*Л.С.:* " + variety.avers + "\n\n"
-        fullDescription = fullDescription + "*Л.С.:* " + variety.avers + "\n\n"
+        result.write(("**Л.С.:** " + variety.avers + "\n\n").encode('utf-8'))
 
     if "" != variety.revers:
-        tableOfContents = tableOfContents + "*О.С.:* " + variety.revers
-        fullDescription = fullDescription + "*О.С.:* " + variety.revers + "\n\n"
+        result.write(("**О.С.:** " + variety.revers + "\n\n").encode('utf-8'))
 
-    if "" == variety.avers and "" == variety.revers:
-        tableOfContents = tableOfContents + "перейти к подробному описанию"
-
-    tableOfContents = tableOfContents + "](#" + variety.id + ")"
+    if "" != variety.mint:
+        result.write(("**Место чеканки:** " + variety.mint + "\n\n").encode('utf-8'))
 
     if "" != variety.comment and variety.comment:
-        fullDescription = fullDescription + "*Комментарий:* " + variety.comment + "\n\n"
+        result.write(("**Комментарий:** " + variety.comment + "\n\n").encode('utf-8'))
 
     if 0 != len(variety.references):
-        fullDescription = fullDescription + "*Разновидность описана:* "
-        for ref in variety.references:
-            fullDescription = fullDescription + ref.srid
-            fullDescription = fullDescription + " " + ref.number
-            if ref.rarity:
-                fullDescription = fullDescription + " " + ref.rarity
-            if ref.comment:
-                fullDescription = fullDescription + ref.comment
-            fullDescription = fullDescription + "; "
+        result.write(("**Разновидность описана:** ").encode('utf-8'))
 
+        for ref in variety.references:
+            result.write(ref.srid.encode('utf-8'))
+            result.write((" " + ref.number).encode('utf-8'))
+
+            if ref.rarity:
+
+                result.write((", " + ref.rarity).encode('utf-8'))
+
+            if ref.comment:
+
+                result.write((" *" + ref.comment + "*").encode('utf-8'))
+
+            result.write(("; ").encode('utf-8'))
+
+        result.write("\n\n".encode('utf-8'))
 
     if 0 != len(variety.pictures):
         for pict in variety.pictures:
-            pictname =  pict.id + ".jpg"
+            pictname = pict.id + ".jpg"
             shutil.copy(CVBAPI.base_root_dir + "images/" + pictname, path + "images/"+ pictname)
-            fullDescription = fullDescription + "![Alt text](images/" + pictname
-            fullDescription = fullDescription + " \"Источник: " + pict.source + "\")\n\n"
+            result.write(("![**Не удалось подставить картинку " + pictname + "**](images/" + pictname).encode('utf-8'))
+            result.write((" \"Источник: " + pict.source + "\")\n\n").encode('utf-8'))
 
 
-    tableOfContents = tableOfContents + "|" + variety.varityType
 
 
-result = open(path+"result.md", "wb")
-
-#todo: add table of context
-result.write(tableOfContents.encode('utf-8'))
-result.write(fullDescription.encode('utf-8'))
 
 #todo: add references
 
