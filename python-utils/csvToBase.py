df067e333cd19422078d6b6791b223b892064689
Alexander Kazanskiy <kazansky85@gmail.com>
2015-06-28 21:04:36 +0300

    pithon-utils: more abstract csvToBase import
    Now it's using field names instead colum number and can read csv without
    all field names

--- python-utils/csvToBase.py
+++ python-utils/csvToBase.py
@@ -4,61 +4,56 @@ __author__ = 'aknew'
 import csv
 import CVBAPI
 from os.path import dirname
+from collections import defaultdict
+
+# Hint: csv struct:
+# first row is header, it can be one of tavle Vatieties field name, "picture" or "picture_source"
+# otherwise header is reference reduction and all
 
 
-def import_csv(path, type):
+def import_csv(path, typeID):
 
     base_path = dirname(path)
 
     Variaties = []
 
-    firstAuthor = 11
-
-    Authors = []
-
     with open(path, 'rt', encoding="utf-8") as csvfile:
-        varList = csv.reader(csvfile)
-        for index, row in enumerate(varList):
+        varList = csv.DictReader(csvfile)
+        field_names = ["varietyType","year","mintmark","mint","avers","revers","edge","picture","picture_source","rarity","comment"]
+        Authors = [n for n in varList.fieldnames if n not in field_names]
 
-            if 0 == index:
-                # header
-                for i in range(firstAuthor, len(row)):
-                    Authors.append(row[i])
-                continue
+        for row in varList:
 
             variety = CVBAPI.Variety()
-            variety.typeId = type
+            variety.typeId = typeID
 
-            variety.varietyType = row[0]
-            variety.year = row[1]
-            variety.mintmark = row[2]
-            variety.mint = row[3]
-            variety.avers = row[4]
-            variety.revers = row[5]
-            variety.edge = row[6]
+            row = defaultdict(lambda: "", row)
 
-            if "" != row[7]:
-                pictures = row[7].split("|")
-                valLen=len(pictures)
-                if valLen > 1:
-                    sources = row[8].split("|")
-                    for index, filename in enumerate(pictures):
-                        pict = CVBAPI.CoinPicture(base_path + "\\" + filename, sources[index], variety.id)
-                        variety.pictures.append(pict)
+            variety.varietyType = row["varietyType"]
+            variety.year = row["year"]
+            variety.mintmark = row["mintmark"]
+            variety.mint = row["mint"]
+            variety.avers = row["avers"]
+            variety.revers = row["revers"]
+            variety.edge = row["edge"]
 
-                else:
-                    pict = CVBAPI.CoinPicture(base_path + "\\" + row[7], row[8], variety.id)
+            if "" != row["picture"]:
+                pictures = row["picture"].split("|")
+                sources = row["picture_source"].split("|")
+                for index, filename in enumerate(pictures):
+                    pict = CVBAPI.CoinPicture(base_path + "\\" + filename, sources[index], variety.id)
                     variety.pictures.append(pict)
 
-            variety.rarity = row[9]
-            variety.comment = row[10]
 
-            for i in range(firstAuthor, len(row)):
-                if "" != row[i]:
-                    values = row[i].split("|")
+            variety.rarity = row["rarity"]
+            variety.comment = row["comment"]
+
+            for author in Authors:
+                if "" != row[author]:
+                    values = row[author].split("|")
                     ref = CVBAPI.SourceRef()
                     ref.varID = variety.id
-                    ref.srid = Authors[i-firstAuthor]
+                    ref.srid = author
                     ref.number = values[0]
                     valLen = len(values)
                     if valLen>1:
