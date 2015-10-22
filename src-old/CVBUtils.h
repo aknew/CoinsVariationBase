1738c27bfd9bb55e216b5bcf18571fb56ecf9a3b
Alexander Kazanskiy <kazansky85@gmail.com>
2015-10-22 23:04:40 +0300

    remove unused legacy files

--- src-old/CVBUtils.h
+++
@@ -1,8 +0,0 @@
-#ifndef CVBUTILS_H
-#define CVBUTILS_H
-#include <QString>
-
-void CVBFromQmlFilePath(QString *str);
-void CVBSaveStringToFile(const QString &stringToSave, const QString &filePath);
-
-#endif // CVBUTILS_H
--- src-old/comboboxdescription.cpp
+++
@@ -1,5 +0,0 @@
-#include "comboboxdescription.h"
-
-ComboBoxDescription::ComboBoxDescription()
-{
-}
