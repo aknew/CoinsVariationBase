43c76df82edad4cc4196b7e8980412cfc335f5da
Alexander Kazanskiy <kazansky85@gmail.com>
2016-03-29 23:38:02 +0300

    fix no image loading

--- src/CBImageProvider.cpp
+++ src/CBImageProvider.cpp
@@ -30,11 +30,9 @@ QPixmap CBImageProvider::requestPixmap(const QString& id, QSize* size, const QSi
     if (result.isNull()){
         QFileIconProvider provider;
         QIcon icon = provider.icon(fullFilePath);
-        if (icon.isNull()){ // FIXME: not null, but icon is wrong
+        result = icon.pixmap(90,90);
+        if (result.isNull()){
             result=QPixmap("://no_image.png");
-        }
-        else{
-            result = icon.pixmap(90,90);
         }
     }
 
