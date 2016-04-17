274a796aeefb5bc67ec2a1d029cbd5a91f78e331
Alexander Kazanskiy <kazansky85@gmail.com>
2016-04-17 19:48:01 +0300

    add doxygen description to CBUtils

--- src/CBUtils.h
+++ src/CBUtils.h
@@ -4,9 +4,15 @@
 
 ///* \brief module for some small functions which are used in several classes
 namespace CBUtils {
+/**
+ * @brief reduce file:// from pathes
+ * @param str - string to reduce
+ * Qml uses path with file:// protocol in the beginning, but qt c++ don't use it, so I need to reduse protocol path
+ * Also for windows FileDialogs add / to the beginning (i.e. full path looks "file:///C:/test.txt"), but, for example, Qfile can't read these pathes
+ */
 void FromQmlFilePath(QString *str);
-void SaveStringToFile(const QString &stringToSave, const QString &filePath);
-void collectTranslation(const QString &original);
+void SaveStringToFile(const QString &stringToSave, const QString &filePath); /// saving string as text file
+void collectTranslation(const QString &original); ///save phrases which need to be translated to specific files, uses for collect those phrases
 
 /** \brief recursivly coping files and dirs
  *  original code: https://gist.github.com/ssendeavour/7324701
