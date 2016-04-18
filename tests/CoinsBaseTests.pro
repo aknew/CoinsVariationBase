b1908b069130e93f62b4d8828ed6ac33cde7cbcf
Alexander Kazanskiy <kazansky85@gmail.com>
2016-04-18 23:03:58 +0300

    prepare to add tests

---
+++ tests/CoinsBaseTests.pro
@@ -0,0 +1,14 @@
+QT += core testlib
+QT -= gui
+
+CONFIG += c++11
+
+TARGET = CoinsBaseTests
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+HEADERS +=
---
+++ tests/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // remove old test base with unknown state
+
+    QDir dir("TestBase");
+    if (!dir.removeRecursively()){
+        qWarning() << "Can't remove old base";
+    }
+
+    //extract testBase in known default state, I used 7zip because it always stays on my PCs
+
+    system("\"c:\\Program Files\\7-Zip\\7z.exe\" x ../testData/TestBase.zip");
+
+    qInfo() << "Base has been prepared, starting tests";
+
+    return 0;
+}
