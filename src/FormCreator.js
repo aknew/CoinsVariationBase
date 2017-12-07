7b9eca2f5f2fefe324839f40a48764e28cc6e0ad
Alexander Kazansky <kazansky85@gmail.com>
2017-12-08 00:22:30 +0300

    fixed listview cell heigth

--- src/FormCreator.js
+++ src/FormCreator.js
@@ -31,7 +31,7 @@ function createListForm(node) {
     qmlString += "      Item {\n"+
                  "          id: delegateItem;\n"+
                  "          width: listView.width;\n"+
-                 "          height: 190\n";
+                 "          height: Math.max(190 ,topLayout.height+10);\n";
 
     qmlString += "          BackgroundRect{selected:(index===mainRect.firstIndex)}\n";
     qmlString += "          MouseArea {anchors.fill: parent\n"+
@@ -55,7 +55,7 @@ function createListForm(node) {
         qmlString += "        Image {\n"+
                      "            id: image\n"+
                      "            source: \"image://imageProvider/\" + id + \"/main.thumbnail.jpg\"\n"+
-                     "            height: parent.height-10\n"+
+                     "            height: 180\n"+
                      "            width: height*2\n"+
                      "            sourceSize.height: height\n"+
                      "            sourceSize.width: width\n"+
